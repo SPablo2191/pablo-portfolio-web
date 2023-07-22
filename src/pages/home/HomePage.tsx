8d30964933cb5d82e482524eb6e69fea72973815
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-22 00:09:06 -0300

    fix: facts section working

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,15 @@
+import { Facts } from "./components/Facts/FactsComponent";
 import Header from "./components/Header/HeaderComponent";
 
 function Home() {
   return (
-    <Header></Header>
+    <>
+      <Header></Header>
+      <hr
+        className="w-48 h-1 mx-auto my-8 bg-blue-600 dark:bg-blue-400 border-0 rounded md:my-10"
+      />
+      <Facts></Facts>
+    </>
   );
 }
 export default Home;
--- src/pages/home/components/Facts/FactsComponent.tsx
+++ src/pages/home/components/Facts/FactsComponent.tsx
@@ -1,2 +1,11 @@
-
-export {}
+function Facts(){
+    return(
+        <div
+        className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
+      >
+        <div className="md:grid grid-cols-2 gap-2">
+        </div>
+      </div>
+    );
+}
+export {Facts}
