23c4fab09ef6bf9441b603b9ab422bd4ffc5c8d6
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-22 01:42:12 -0300

    fix: facts fetch data

--- src/components/ui/bigCard/bigCardComponent.tsx
+++ src/components/ui/bigCard/bigCardComponent.tsx
@@ -1,23 +1,23 @@
-function BigCard({ title, imageUrl, description }: any){
-    return (
-        <div
-        className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
-      >
-        <div className="md:grid grid-cols-2 gap-2">
-          <div className="fade_from_left">
-            <img
-              src={imageUrl}
-              className="rounded mx-auto d-block about_me_picture"
-            />
-          </div>
-          <div className="fade_from_right">
-            <h2 className="text-2xl font-bold mb-2 text-center">{title}</h2>
-            <p className="text-lg text-justify">
-              {description}
-            </p>
-          </div>
+function BigCard({ title, imageUrl, description }: any) {
+  return (
+    <div
+      className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
+    >
+      <div className="md:grid grid-cols-2 gap-2">
+        <div className="fade_from_left">
+          <img
+            src={imageUrl}
+            className="rounded mx-auto d-block about_me_picture"
+          />
+        </div>
+        <div className="fade_from_right">
+          <h2 className="text-2xl font-bold mb-2 text-center">{title}</h2>
+          <p className="text-lg text-justify">
+            {description}
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
-export {BigCard}
+export { BigCard }
--- src/pages/home/components/Facts/FactsComponent.tsx
+++ src/pages/home/components/Facts/FactsComponent.tsx
@@ -1,11 +1,25 @@
-function Facts(){
-    return(
+import React, { useEffect, useState } from 'react';
+import { BigCard } from '../../../../components/ui/bigCard/bigCardComponent';
+import aboutMePicture from '../../../../assets/images/about-me-picture.jpeg'
+const apiUrl = process.env.REACT_APP_API_URL;
+function Facts() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch(apiUrl + 'facts')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
+    return (
         <div
-        className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
-      >
-        <div className="md:grid grid-cols-2 gap-2">
+            className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
+        >
+            {data.map((item: any) => (
+                <div className="">
+                    <BigCard title={item.title} imageUrl={aboutMePicture} description={item.description} ></BigCard>
+                </div>
+            ))}
         </div>
-      </div>
     );
 }
-export {Facts}
+export { Facts }
