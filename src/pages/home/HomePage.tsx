c28a145f095d4d17270ef90f224578352b93dd7d
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-22 12:31:01 -0300

    fix: dark theme working

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,13 +3,13 @@ import Header from "./components/Header/HeaderComponent";
 
 function Home() {
   return (
-    <>
+    <div className="dark:text-white text-gray-800">
       <Header></Header>
       <hr
         className="w-48 h-1 mx-auto my-8 bg-blue-600 dark:bg-blue-400 border-0 rounded md:my-10"
       />
       <Facts></Facts>
-    </>
+    </div >
   );
 }
 export default Home;
