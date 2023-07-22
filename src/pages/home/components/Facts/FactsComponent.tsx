a8fffdf2ff7cdf435f610c39ec02b98d7f123a5e
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-22 01:58:34 -0300

    fix: dark mode and fact key error

--- src/pages/home/components/Facts/FactsComponent.tsx
+++ src/pages/home/components/Facts/FactsComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BigCard } from '../../../../components/ui/bigCard/bigCardComponent';
 import aboutMePicture from '../../../../assets/images/about-me-picture.jpeg'
+import { Fact } from '../../../../models/fact.interface';
 const apiUrl = process.env.REACT_APP_API_URL;
 function Facts() {
     const [data, setData] = useState([]);
@@ -14,9 +15,9 @@ function Facts() {
         <div
             className="bg-white m-4 dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"
         >
-            {data.map((item: any) => (
-                <div className="">
-                    <BigCard title={item.title} imageUrl={aboutMePicture} description={item.description} ></BigCard>
+            {data.map((item: Fact) => (
+                <div key={item.id}>
+                    <BigCard title={item.title}  imageUrl={aboutMePicture} description={item.description} ></BigCard>
                 </div>
             ))}
         </div>
