660654181363cfb2e2f56044778a095d3ee0b323
SPablo2191 <pablosandoval2191@gmail.com>
2023-07-24 20:26:51 -0300

    fix: margin between title and items

--- src/components/skillCard/SkillCardComponent.tsx
+++ src/components/skillCard/SkillCardComponent.tsx
@@ -6,7 +6,7 @@ function SkillCard({ name, items }: any) {
         <div
             className="bg-white m-4 text-center dark:bg-slate-900 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl fade_from_below"
         >
-            <h2 className="text-3xl font-bold">{name}</h2>
+            <h2 className="text-3xl font-bold mb-3">{name}</h2>
             <div className="md:grid grid-cols-3">
                 {items.map((item: Technology) => (
                     <div key={item.id}>
